b37a597b8685b23155221bd145f0082176b67f94
rahulmanas <rahulmns31@gmail.com>
2023-06-28 17:29:37 +0530

    Publish changes (#2)
    * Publish changes
    
    * Publish bug fix

---
+++ components/lib/index.js
@@ -0,0 +1,8 @@
+// This file lets you publish the components
+// that you want to be imported after installing the package
+
+import Modal from "../Modal/Modal";
+import MetamaskCard from "../connectorCard/MetamaskCard";
+import ChooseToken from "../ChooseToken";
+
+export { Modal, MetamaskCard, ChooseToken };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import { AppProps } from "next/app";
 import "../styles/index.scss";
 import { ModalProvider } from "../hooks/useModalManager";
 
+import ErrorBoundary from "../components/ErrorBoundary";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ModalProvider>
-      <Component {...pageProps} />
-    </ModalProvider>
+    <ErrorBoundary>
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
+    </ErrorBoundary>
   );
 }
 
