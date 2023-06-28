cf8a9175d220c0742f805176f7dabfbdf6121dce
rahulmanas <rahulmns31@gmail.com>
2023-06-28 17:54:44 +0530

    Complie bug fix

--- components/TokenSwap.tsx
+++ components/TokenSwap.tsx
@@ -155,9 +155,9 @@ const TokenSwap = ({ isActive, balance }) => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="#5D6785"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <line x1="12" y1="5" x2="12" y2="19"></line>
               <polyline points="19 12 12 19 5 12"></polyline>
